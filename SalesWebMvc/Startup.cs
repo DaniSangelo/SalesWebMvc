4cef76331901bafaf09b4e18ec09a11e417a7f15
daniel sangelo <danielsangelo@hotmail.com>
2020-12-10 19:23:48 -0300

    simple search

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -8,9 +8,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Localization;
-using SalesWebMvc.Data;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 using System.Collections.Generic;
+using SalesWebMvc.Data;
 
 namespace SalesWebMvc
 {
@@ -43,8 +44,9 @@ namespace SalesWebMvc
                     builder.MigrationsAssembly("SalesWebMvc")));
 
             services.AddScoped<SeedingService>(); // permite que seja injetado em outras classes
-            services.AddScoped<SellerService>(); // permite que seja injetado em outras classes
-            services.AddScoped<DepartmentService>(); // permite que seja injetado em outras classes
+            services.AddScoped<SellerService>();
+            services.AddScoped<DepartmentService>(); 
+            services.AddScoped<SalesRecordService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
